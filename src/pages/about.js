513e8b321760225ecede1669829cb7b088425efc
jaygala24 <jaygala24@gmail.com>
2019-09-05 20:43:20 +0530

    Working with Gatsby Pages

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About Me</h1>
+      <p>I'm a student pursuing CS.</p>
+    </div>
+  )
+}
+
+export default AboutPage
